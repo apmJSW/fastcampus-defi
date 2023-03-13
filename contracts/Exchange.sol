8659ac540ff0e3c3ac7ba8af0a560dab0eaabb54
seungwoo <jsu7375@gmail.com>
2023-03-13 22:50:59 +0900

    edit source

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,34 +8,34 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Exchange {
     IERC20 token;
 
-    constructor (address _token) {
-        token = IERC20(_token);
-    }
-    
-    function addLiquidity(uint256 _tokenAmount) public payable {
-        token.transferFrom(msg.sender, address(this), _tokenAmount);
-    }
+  constructor (address _token) {
+    token = IERC20(_token);
+  }
   
-    // ETH -> ERC20
-    function ethToTokenSwap() public payable {
-        // calculate amount out
-        uint256 amountOut = getOutputAmount(msg.value, IERC20(token).balanceOf(address(this)), address(this).balance - msg.value);
+  function addLiquidity(uint256 _tokenAmount) public payable {
+    token.transferFrom(msg.sender, address(this), _tokenAmount);
+  }
 
-        //transfer token out
-        IERC20(token).transfer(msg.sender, amountOut);
-    }
+  // ETH -> ERC20
+  function ethToTokenSwap() public payable {
+    // calculate amount out
+    uint256 amountOut = getOutputAmount(msg.value, IERC20(token).balanceOf(address(this)), address(this).balance - msg.value);
 
-    function getPrice(uint256 inputReserve, uint256 outputReserve) public pure returns (uint256) {
-      uint256 numerator = inputReserve;
-      uint256 denominator = outputReserve;
-      return numerator / denominator;
-    }
-    
-    function getOutputAmount(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve) public pure returns (uint256) {
-        uint256 numerator = (inputAmount * outputReserve);
-        uint256 denominator = (inputReserve + inputAmount);
-        return numerator / denominator;
-    }
+    //transfer token out
+    IERC20(token).transfer(msg.sender, amountOut);
+  }
+
+  function getPrice(uint256 inputReserve, uint256 outputReserve) public pure returns (uint256) {
+    uint256 numerator = inputReserve;
+    uint256 denominator = outputReserve;
+    return numerator / denominator;
+  }
+  
+  function getOutputAmount(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve) public pure returns (uint256) {
+    uint256 numerator = (inputAmount * outputReserve);
+    uint256 denominator = (inputReserve + inputAmount);
+    return numerator / denominator;
+  }
 
 }
 
