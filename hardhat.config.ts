9f59d57ac6f37d3f884286276164b1c413e06c55
seungwoo <jsu7375@gmail.com>
2023-03-28 00:46:23 +0900

    add deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,24 @@
 import "@nomiclabs/hardhat-waffle";
 import "@nomicfoundation/hardhat-toolbox";
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      gas: 10000000,
+      gasPrice: 875000000
+    },
+    goerli: {
+      url: process.env.ALL_THAT_NODE_API_KEY,
+      accounts: [process.env.METAMASK_PRIVATE_KEY as string]
+    }
+  }, etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,23 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  console.log("Deploying contracts with the account: ", deployer.address);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Factory = await ethers.getContractFactory("Factory");
+  const contract = await Factory.deploy();
 
-  await lock.deployed();
+  const GrayToken = await ethers.getContractFactory("Token");
+  const grayTokenContract = await GrayToken.deploy("GrayToken", "GRAY", 1000);
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("FactoryContract deployed at: ", contract.address );
+  console.log("GrayTokenContract deployed at: ", grayTokenContract.address );
+
+  const Exchange = await ethers.getContractFactory("Exchange");
+  const exchageContract = await Exchange.deploy(grayTokenContract.address);
+  
+  console.log("ExchangeContract deployed at: ", exchageContract.address );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
